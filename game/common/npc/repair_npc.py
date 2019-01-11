a63397b65d87df632241e7bc165928d86d637f72
sirdude103 <sirdude103@gmail.com>
2019-01-10 20:05:04 -0600

    repair npc now moves between stations

--- game/common/npc/repair_npc.py
+++ game/common/npc/repair_npc.py
@@ -8,6 +8,24 @@ class RepairNPC(NPC):
 
     def take_turn(self, universe):
 
+        # if at heading, clear heading
+        if (self.heading is not None
+                and self.heading[0] == self.ship.position[0]
+                and self.heading[1] == self.ship.position[1]):
+            self.heading = None
+
+        # choose a new heading if we don't have one
+        if self.heading is None:
+            # self.heading = ( random.randint(0, WORLD_BOUNDS[0]), random.randint(0, WORLD_BOUNDS[1]))
+            self.heading = random.choice(
+                universe.get(ObjectType.station) +
+                universe.get(ObjectType.secure_station) +
+                universe.get(ObjectType.black_market_station)
+            ).position
+
+        # move towards heading
+        self.move(*self.heading)
+        
         self.repair(self, random.randint(self.max_hull))
 
 
