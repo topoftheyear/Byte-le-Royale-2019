d64a478f827ad72d3f655903c7e065910d1223ea
topoftheyear <topoftheyear@Gmail.com>
2019-01-11 16:25:01 -0600

    build_WIN.bat once again works but now in the scripts folder.

--- scripts/build_WIN.bat
+++ scripts/build_WIN.bat
@@ -1,5 +1,7 @@
 del /q *.pyz
 
-xcopy /s/e/i "game" "wrapper/game"
-python -m zipapp wrapper -o br_launcher.pyz
-del /q/s "wrapper/game"
+xcopy /s/e/i "../game" "../wrapper/game"
+python -m zipapp "../wrapper" -o "../br_launcher.pyz"
+del /q/s "../wrapper/game"
+
+pause
