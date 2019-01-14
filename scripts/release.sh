5ee4977063ab0f08e838e102b270cdb97081de2f
JordanGoetze <jghibiki.games@gmail.com>
2019-01-14 14:07:29 -0600

    Bump version to 0.93\nrelease notes

--- scripts/release.sh
+++ scripts/release.sh
@@ -42,7 +42,7 @@ echo $response
 
 # parse out upload url
 set -x
-upload_url=$(echo $response | tr , \\n | grep "upload_url" | cut -d ":\"" -f 2 ) #| cut -d / -f 1-8) + "/assets?name=br_launcher.pyz"
+upload_url=$(echo $response | tr , \\n | grep "upload_url" | cut -d ":" -f 2-3 ) #| cut -d / -f 1-8) + "/assets?name=br_launcher.pyz"
 echo "Upload URL: $upload_url"
 
 
