f72a8dc944950355263acc9f477cad7f83fa8d22
Eric M <EricWM612@Gmail.com>
2019-02-08 22:41:20 -0600

    Added boundary_break AI to test death upon leaving the world. Tests confirm functionality, completing #149.

---
+++ game/common/npc/test/boundary_break.py
@@ -0,0 +1,55 @@
+import random
+from game.common.enums import *
+from game.common.npc.npc import NPC
+from game.utils.helpers import *
+from game.client.user_client import UserClient
+from game.config import *
+import game.utils.filters as F
+
+
+class BoundaryBreak(NPC):
+
+    def __init__(self, ship):
+        UserClient.__init__(self)
+        self.ship = ship
+        self.ship_id = ship.id
+
+        self.action = None
+        self.target = None
+        self.material = None
+
+        self.type = None
+        self.level = None
+
+        self.bought = False
+
+        self.fields = None
+        self.stations = None
+
+        self.previous_position = None
+        self.edition = random.randint(0, 1)
+        self.subedition = random.randint(0, 1)
+
+    def team_name(self):
+        return f"Boundary Breaker# {random.randint(1,1000)}"
+
+    def take_turn(self, universe):
+        if self.ship.is_alive():
+            if self.edition:
+                if self.subedition:
+                    print("Edging zeroes")
+                    self.move(0, 0)
+                else:
+                    print("Edging max")
+                    self.move(WORLD_BOUNDS[0], WORLD_BOUNDS[1])
+            else:
+                if self.subedition:
+                    print("I'm leaving max!")
+                    self.move(WORLD_BOUNDS[0]+1, WORLD_BOUNDS[1]+1)
+                else:
+                    print("I'm leaving min!")
+                    self.move(-1, -1)
+        else:
+            print("D'oh, I'm dead!")
+
+        return self.action_digest()
